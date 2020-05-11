b68ec0d0abcfb02da27ed2566d823066fe3e3a53
Helton Denis <heltondeniss@gmail.com>
2020-05-10 22:02:19 -0300

    New controllers

---
+++ app/Controllers/Http/ApartamentController.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const Apartament = use('App/Models/Apartament')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with apartaments
+ */
+class ApartamentController {
+  /**
+   * Show a list of all apartaments.
+   * GET apartaments
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const apartaments = await Apartament.all();
+
+    return apartaments;
+  }
+
+  /**
+   * Render a form to be used for creating a new apartament.
+   * GET apartaments/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new apartament.
+   * POST apartaments
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single apartament.
+   * GET apartaments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing apartament.
+   * GET apartaments/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update apartament details.
+   * PUT or PATCH apartaments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a apartament with id.
+   * DELETE apartaments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ApartamentController
