1a3efccfa8ea503f4be04e65b35b1cabdf62e9b6
Helton Denis <heltondeniss@gmail.com>
2020-05-10 22:04:02 -0300

    Group routes for apartaments with auth middleware

--- start/routes.js
+++ start/routes.js
@@ -3,4 +3,14 @@
 const Route = use('Route')
 const Database = use('Database')
 
+/** Login and Authentication */
 Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.store')
+
+/** Routes Apartaments */
+Route.group(() => {
+  Route.get('/apartaments', 'ApartamentController.index')
+}).middleware(['auth'])
+
+
+
