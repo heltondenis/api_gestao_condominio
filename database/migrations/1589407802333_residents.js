aeda4226cc4abb78d1e948d1e4e0c7cab0c199b5
Helton Denis <heltondeniss@gmail.com>
2020-05-13 20:23:24 -0300

    Maintenance of migrations already active

--- database/migrations/1589407802333_residents.js
+++ database/migrations/1589407802333_residents.js
@@ -7,9 +7,17 @@ class Residents extends Schema {
   up () {
     this.create('residents', (table) => {
       table.increments()
+      table
+      .integer('apartament_id')
+      .unsigned()
+      .references('id')
+      .inTable('apartaments')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
       table.string('full_name').notNullable()
       table.date('date').notNullable()
       table.string('cpf').notNullable()
+      table.string('fone').notNullable()
       table.string('email').notNullable()
       table.boolean('admin_status').notNullable()
       table.timestamps()
